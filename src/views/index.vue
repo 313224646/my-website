5f90cb2b92a4a7f9760abeaefc55e00458bf08b5
Shanghai Wu <18620800179@163.com>
2019-05-14 18:47:12 +0800

    update

---
+++ src/views/index.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="index">
+    <!-- <p>Chat, future me.</p> -->
+    <base-field :value.sync="value1" placeholder="说点什么吧, 老兄."/>
+    <base-field :value.sync="value2" placeholder="说点什么吧, 老兄."/>
+    <base-field :value.sync="value3" placeholder="说点什么吧, 老兄."/>
+    <base-field :value.sync="value4" placeholder="说点什么吧, 老兄."/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  components: {
+    'base-field': () => import('@/components/base/base-field')
+  },
+  data () {
+    return {
+      value1: '',
+      value2: '',
+      value3: '',
+      value4: '',
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+</style>
